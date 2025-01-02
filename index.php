d6daf88545130cc4d8a772b0cff243acfe83d6c5
Jual Rana <contactjual@gmail.com>
2025-01-02 12:56:42 +0600

    date advence

--- index.php
+++ index.php
@@ -4,8 +4,23 @@
 
 date_default_timezone_set("Asia/Dhaka");
 
-echo "Today is_ ".date("D h: i: s a _ d M, Y");
+echo "Today is_ ".date("D h: i: s a _ d M, Y") ."<br>";
 
+
+
+// string to time function 
+
+$start_date = strtotime("sunday");
+$end_date = strtotime("+1 week", $start_date); // 1 week pore sunday er date ta dekhabe
+echo date("d-m-y",  $end_date)."<br>";
+
+
+$end_date = strtotime("+6 weeks", $start_date);
+
+while($start_date < $end_date) {
+    echo date("M d", $start_date)."<br>";
+    $start_date = strtotime("+1 week", $start_date);
+}
 
 
 ?>
